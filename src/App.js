0971ef6e89bd47c97d4e2064b4e586177a1ab3ef
AnnaVarda <77052038+AnnaVarda@users.noreply.github.com>
2021-11-17 18:16:05 +0200

    Update dom from v5 to v6

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
 
 function App() {
   return (
-  <>
-    < BrowserRouter>
+  <div>
+    
       <Navbar />
       <Routes>
         <Route  path='/' element= {<Home/>} />
       </Routes>
-    </BrowserRouter>
-  </>
+    
+  </div>
   );
 }
 
