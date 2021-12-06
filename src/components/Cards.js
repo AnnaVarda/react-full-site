5e966f3f485909fe4b01a3b43bfb3d6b07803936
AnnaVarda <77052038+AnnaVarda@users.noreply.github.com>
2021-12-06 19:15:44 +0200

    Cards CSS

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,12 +5,13 @@ import { Link } from 'react-router-dom';
 function Cards(props) {
     return (
         <div className="card">
-				<Link to="/services">
+				
           <div className="card__body">
 						<img src={props.img} alt="" className="card__image" />		
 							<h2 className="card__title">{props.title}</h2>
 							<p className="card__description">{props.description}</p>
 					</div>
+				<Link to="/services">
 					<button className="card__btn">Learn more</button>
 				</Link>
 				</div>
