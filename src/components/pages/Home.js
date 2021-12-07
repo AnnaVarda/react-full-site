2671c42d03699e79c257e04089305941a5a23df3
AnnaVarda <77052038+AnnaVarda@users.noreply.github.com>
2021-12-07 11:56:12 +0200

    Modify cards

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,26 +4,28 @@ import HeroSection from '../HeroSection';
 import Cards from '../Cards';
 import '../Cards.css';
 
+
+
 function Home() {
     return (
       <>
           <HeroSection />
         <div className="wrapper">
           <Cards
-          img="https://www.ewoman.gr/sites/default/files/inline-images/3-ewoman_8.jpg"
-          title="The everyday Salad"
-          description="Dance with me and LEVEL UP your life"
+          img="https://www.ewoman.gr/sites/default/files/styles/703xh/public/2019-11/5-ewoman.jpg?itok=huR3qwMA"
+          title="Social Dance Classes"
+          description="Salsa - Bachata - Kizomba"
            />
 
           <Cards
-          img="https://www.ewoman.gr/sites/default/files/inline-images/3-ewoman_8.jpg"
-          title="The everyday Salad"
-          description="Dance with me and LEVEL UP your life" />
+          img="https://scontent.fath4-2.fna.fbcdn.net/v/t1.15752-9/79929672_1450528118461946_2849707498066149376_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=ae9488&_nc_ohc=-ACamvwoLDoAX9r0n5i&_nc_ht=scontent.fath4-2.fna&oh=32f524337180a0a65f2f4310e21a3d7d&oe=61D56589"
+          title="Fitness Classes"
+          description="Pilates - Flexibility - Rehab" />
 
           <Cards
-          img="https://www.ewoman.gr/sites/default/files/inline-images/3-ewoman_8.jpg"
-          title="The everyday Salad"
-          description="Dance with me and LEVEL UP your life" />
+          img=".images/IMG_5835.JPG"
+          title="Dance Therapy Classes"
+          description="Primitive Expression - Afro Purcussion - Ecstatic" />
         </div>
       </>
     );
